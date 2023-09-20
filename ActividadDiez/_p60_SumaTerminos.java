4ad239ec9536ced170301ba1dd15c547e4f2cb1f
migueRivera <ultralitogodofredo@gmai.com>
2023-09-19 18:37:24 -0600

    c1

---
+++ ActividadDiez/_p60_SumaTerminos.java
@@ -0,0 +1,35 @@
+//Suma de n términos de factorial
+//Miguel Esaú Rivera Román
+//19-09-23          Version 1.0.0
+
+import java.util.Scanner;
+
+public class _p60_SumaTerminos {
+    public static void main(String[] args) {
+        int i,j,n;
+        float f, r;
+        char resp;
+        Scanner obj = new Scanner(System.in);
+
+        do {
+            System.out.print("\033[H\033[2J");System.out.flush();
+            r=0;
+            System.out.print("Cuantos números ? "); 
+            n = obj.nextInt();
+
+            for(i=1; i<=n; i++) {
+                f=1;
+                
+                for(j=1; j<=i; j++) {
+                    f *= j;
+                }
+                System.out.printf(" 1 / %,.2f +",f);
+                r += (1 / f);
+            }
+            System.out.printf("= %,.2f\n",r);
+            System.out.print("\nDeseas continuar (S/N) ?");
+            resp=Character.toUpperCase(obj.next().charAt(0));
+        } while( resp!= 'N');
+        System.out.println("\nProceso terminado ..");
+    }
+}
